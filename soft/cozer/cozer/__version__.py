24f0a214de8038ab4f2fbb43f912816f1461a47c
pearu <pearu@4014b329-3e42-3da1-6392-020ce67ceb2a>
2006-07-19 10:29:33 +0000

    Lisa svn version tar faililie.

--- soft/cozer/cozer/__version__.py
+++ soft/cozer/cozer/__version__.py
@@ -1,1 +1,6 @@
 __version__ = '2.6'
+try:
+    from __svn_version__ import version
+    __version__ += '_svn' + version
+except ImportError:
+    pass
--- soft/cozer/setup.py
+++ soft/cozer/setup.py
@@ -5,8 +5,8 @@ setup.py for building/installing COZER
 Usage:
    python setup.py install
 
-Copyright 2001 Pearu Peterson all rights reserved,
-Pearu Peterson <pearu@cens.ioc.ee>          
+Copyright 2001,2006 Pearu Peterson all rights reserved,
+Pearu Peterson <pearu.peterson@gmail.com>          
 Permission to use, modify, and distribute this software is given under the
 terms of the LGPL.  See http://www.fsf.org
 
@@ -18,7 +18,7 @@ Pearu Peterson
 
 __version__ = "$Id: setup.py,v 1.3 2001/12/25 19:16:40 pearu Exp $"
 
-import sys,os,glob
+import sys,os,glob,re
 glob = glob.glob
 
 from distutils.core import setup
@@ -68,7 +68,21 @@ class my_install_scripts (install_scripts):
                     if not self.dry_run:
                         os.rename (file, new_file)
 
+svn_entries = os.path.join('.svn','entries')
+if os.path.isfile(svn_entries):
+    f = open(svn_entries)
+    m = re.search(r'revision="(?P<revision>\d+)"',f.read())
+    f.close()
+    if m:
+        revision = m.group('revision')
+        svn_version = os.path.join('cozer','__svn_version__.py')
+        f = open(svn_version,'w')
+        f.write('version = %r\n' % (revision))
+        f.close()
+
+sys.path.insert(0,'cozer')
 execfile(os.path.join('cozer','__version__.py'))
+del sys.path[0]
 
 if __name__ == "__main__":
     print 'COZER Version',__version__
@@ -77,7 +91,7 @@ if __name__ == "__main__":
           version=__version__,
           description       = "COZER - COmpetation organiZER",
           author            = "Pearu Peterson",
-          author_email      = "pearu@cens.ioc.ee",
+          author_email      = "pearu.peterson@gmail.com",
           licence           = "GPL",
           long_description  = """\
           COZER is a program for organizing competitive events. In
