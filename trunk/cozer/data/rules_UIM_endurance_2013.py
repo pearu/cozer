b2934a578c6375a0d0c2436d83541c8d7ba5a5be
pearu.peterson <pearu.peterson@4014b329-3e42-3da1-6392-020ce67ceb2a>
2013-06-11 20:32:24 +0000

    Impl LL and LL2 marks. Added endurance rules and fixed append from python.

---
+++ trunk/cozer/data/rules_UIM_endurance_2013.py
@@ -0,0 +1,66 @@
+{
+ 'rules': [['IR', 'IR', ' ', 'Interruption'],
+           ['DS', 'DS', ' ', 'Did not start'],
+
+           ['YC', 'YC', '406.05_{5.1}', 'Yellow card for dangerous driving'],
+           ['RC', 'RC', '406.05_{5.2}', 'Red card for extreme blatant dangerous driving'],
+
+           ['PL8', 'PL8', '902.22_1', 'Fuel infringement'],
+           ['PL5', 'PL5', '902.22_2', 'Race infringements'],
+           ['LL2', 'LL2', '902.22_3', 'Light infringements'],
+           ['PL8', 'PL8', '902.22_4', 'Craning infringements'],
+           ['PL5', 'PL5', '902.22_5', 'Failure to attend scrutineering or drivers briefing'],
+           ['PL5', 'PL5', '902.22_6', 'Flag infringements'],
+           ['PL5', 'PL5', '902.22_7', 'Priority of boats'],
+           ['PL5', 'PL5', '902.33_1', 'For dislodging or destroying the first turn buoy or other buoys of the race course'],
+           ['PL10', 'PL10', '902.33_2', 'For dislodging or destroying the second turn buoy or other buoys of the race course'],
+
+           ['DQ', 'DQ', '206.03', 'Absence of race number or part of that'],
+           ['DQ', 'DQ', '205.02.02.a', 'BAC more than 0.10 0/00 1h before and during testing or race'],
+           ['DQ', 'DQ', '306.06_3', 'Before the start: Infringement of the Rules of Road 312'],
+           ['DQ', 'DQ', '306.08_1', 'Approaching the starting line other than at the right angle...'],
+           ['DQ', 'DQ', '306.09_1', 'Starting before time'],
+           ['DQ', 'DQ', '306.09_2', 'Crossing the starting line during the five minutes'],
+           ['DS', 'DS', '306.09_4', 'The leading boat starting before time in prev. start (restart)'],
+
+           ['DQ', 'DQ', '307.02_2', 'Not keeping a lane start-1st turn'],
+           ['PL', 'PL', '307.02_2', 'Not keeping a lane start-1st turn'],
+           ['YC', 'YC', '307.02_2', 'Not keeping a lane start-1st turn'],
+           ['DQ', 'DQ', '307.04_1', 'Engine running during the 30 secs'],
+           ['LL', 'LL', '307.04_2', 'Engine start with the red light on'],
+           ['LL', 'LL', '307.04_3', 'Engine rotation prior the red lights going off'],
+           ['DQ', 'DQ', '307.04_4', 'Crew member swimming or wading out to a boat'],
+
+           ['DS', 'DS', '311.01.4', 'Caused stoppage in prev. start (restart)'],
+           ['PL', 'PL', '311.01.5', 'Penalty lap from prev. start (restart)'],
+           ['DS', 'DS', '311.01.6', 'Disqualified in prev. start (restart)'],
+           ['DS', 'DS', '311.01.8', "Didn't reach the jetty (restart & electric)"],
+           ['DS', 'DS', '311.02.5', "Didn't scored at the lap before the stoppage (restart)"],
+
+           ['LL', 'LL', '313.02_1', 'Rounding a mark in the wrong way'],
+           ['DQ', 'DQ', '313.02_1', 'Repeated rounding a mark'],
+
+           ['PL', 'PL', '313.03_{3.2}', 'Damage/destroy 1st buoy'],
+           ['DQ', 'DQ', '313.03_{3.4}', 'Damage/destroy/dislodge 2nd or single buoy'],
+
+           ['DQ', 'DQ', '314', 'Fouling competing boats'],
+
+           ['DQ', 'DQ', '317.02_3', 'After finish cutting the course'],
+
+           ['DQ', 'DQ', '318.08', 'A boat or motor is illegal (post-race inspection)'],
+
+           ['DQ', 'DQ', '321.01_2', 'Refusing to produce the engine or the boat'],
+           ['DQ', 'DQ', '321.01_3', 'Attempting to make changes to the engine or hull after race and before technical inspection (OOD, UIM Comm.)'],
+           ['DQ', 'DQ', '321.02_4', 'Visiting pits during the move to parc ferme after the end of final heat'],
+
+           ['DQ', 'DQ', '502.04_4', 'Refusing technical inspection requests after races are over'],
+           ['DQ', 'DQ', '502.07_1', 'Ignoring TO instructions'],
+           ['DQ', 'DQ', '502.07_2', 'A Breach of the technical rules'],
+           ['DQ', 'DQ', '502.08_4', 'Outside assistance on the race course'],
+           ['DQ', 'DQ', '503.03_5', 'Non-confirming automatic throttle shut-off'],
+           ['DQ', 'DQ', '508.08.6', 'The presence of illegal fuel at any time'],
+           ['DQ', 'DQ', '508.10.1', 'Illegal fuel'],
+           
+           ],
+ 'scoringsystem': [20, 17, 15, 13, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]+[1]*15,
+ }
